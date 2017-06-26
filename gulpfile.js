5eced7ef6bbd8212c5cb65b030c0f8ac2b86168b
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-06-26 15:34:35 +0200

    dodano image-min

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+  watch = require('gulp-watch');
+ 
 
 gulp.task('sass', function() {
-    gulp.src('*.scss')
+    gulp.src('*.sass')
         .pipe(sass())
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
-});
+;
+gulp.task('default', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+})
+})
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('*.scss', ['sass']);
+    gulp.watch('app/sass/**/*.sass', ['sass']); 
 })
+
 
